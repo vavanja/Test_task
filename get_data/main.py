7486066ffb89f94d74ecbd1e3796f1f073891a4a
Vova <volodimir979@gmail.com>
2023-03-22 09:15:56 +0200

    initial

---
+++ get_data/main.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+import psycopg2
+from countryinfo import CountryInfo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Population:
+    def __init__(self, url, db_host, db_port, db_name, db_user, db_password):
+        self.url = url
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+
+    def get_data(self):
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        table = soup.find('table', {'class': 'wikitable sortable'})
+        rows = table.find_all('tr')
+
+        count = 0
+
+        for row in rows[3:]:
+            # for row in rows[150:191]:
+            try:
+                cells = row.find_all('td')
+                if len(cells) >= 2:
+
+                    country = cells[0].text.strip()
+
+                    population = cells[1].text
+                    population_ = population.replace(',', '')
+                    population_clear = int(float(population_))
+
+                    region = self.check_region(country).region()
+                    if region:
+                        self.insert_data(country, population_clear, region)
+                    # print(type(region))
+
+                    # self.insert_data(country, population_clear, 'Undefined')
+                continue
+            except Exception as e:
+                cells = row.find_all('td')
+                country = cells[0].text.strip()
+                population = cells[1].text
+                population_ = population.replace(',', '')
+                population_clear = int(float(population_))
+                self.insert_data(country, population_clear, 'Undefined')
+                # print(e, 'Undefined')
+                count += 1
+                continue
+
+        # print(count)
+        print('Done')
+
+    def check_region(self, country):
+        return CountryInfo(country)
+
+    def insert_data(self, country, population, region):
+        conn = psycopg2.connect(
+            host=self.db_host,
+            database=self.db_name,
+            user=self.db_user,
+            password=self.db_password
+        )
+
+        cur = conn.cursor()
+        table_create = ("""CREATE TABLE IF NOT EXISTS information (
+        id SERIAL PRIMARY KEY,
+        country VARCHAR(255) NOT NULL,
+        population INTEGER,
+        region VARCHAR(255)
+         )""")
+
+        cur.execute(table_create)
+        cur.execute('INSERT INTO information (country, population, region) VALUES (%s, %s, %s)',
+                    (country, population, region))
+
+        conn.commit()
+        conn.close()
+
+
+if __name__ == '__main__':
+    scraper = Population('https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population',
+                         db_host='db',
+                         db_port=5432,
+                         db_name='population_db',
+                         db_user='parser_user',
+                         db_password='parser'
+                         )
+    scraper.get_data()
---
+++ print_data/print_data.py
@@ -0,0 +1,66 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class CountryPopulation:
+    def __init__(self, db_name, db_user, db_password, db_host, db_port):
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_host = db_host
+        self.db_port = db_port
+
+    def print_data(self):
+        conn = psycopg2.connect(
+            dbname=self.db_name,
+            user=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port
+        )
+        cur = conn.cursor()
+        table_create = ("""CREATE TABLE IF NOT EXISTS information (
+        id SERIAL PRIMARY KEY,
+        country VARCHAR(255) NOT NULL,
+        population INTEGER,
+        region VARCHAR(255)
+         )""")
+
+        cur.execute(table_create)
+
+        query = ("""
+    SELECT
+      region,
+      SUM(population) AS total_population,
+      MAX(country) FILTER (WHERE population = (SELECT MAX(population) FROM information WHERE region = info.region)) AS largest_country,
+      MAX(population) FILTER (WHERE population = (SELECT MAX(population) FROM information WHERE region = info.region)) AS largest_country_population,
+      MIN(country) FILTER (WHERE population = (SELECT MIN(population) FROM information WHERE region = info.region)) AS smallest_country,
+      MIN(population) FILTER (WHERE population = (SELECT MIN(population) FROM information WHERE region = info.region)) AS smallest_country_population
+    FROM
+      information info
+    GROUP BY
+      region
+""")
+
+        cur.execute(query)
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(f"{row[0]}: {row[1]}| Biggest country: {row[2]} ({row[3]}) | Smallest country: {row[4]} ({row[5]})")
+
+        cur.close()
+        conn.close()
+
+
+if __name__ == '__main__':
+    scraper = CountryPopulation(
+        db_host='db',
+        db_port=5432,
+        db_name='population_db',
+        db_user='parser_user',
+        db_password='parser'
+    )
+    scraper.print_data()
